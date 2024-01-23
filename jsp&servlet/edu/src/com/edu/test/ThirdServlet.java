8c3e99d23b622b141abba09367acefa6f9c9557c
kdhdd <109668066+kdhdd@users.noreply.github.com>
2024-01-23 23:36:41 +0900

    dd
    d

--- jsp&servlet/edu/src/com/edu/test/ThirdServlet.java
+++ jsp&servlet/edu/src/com/edu/test/ThirdServlet.java
@@ -2,20 +2,16 @@ package com.edu.test;
 
 import java.io.*;
 import javax.servlet.*;
-import javax.servlet.http.*;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
 
-@WebServlet("/second")
-public class SecondServlet extends HttpServlet{
-	
+@WebServlet("/third")
+public class ThirdServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		System.out.println("SecondServlet!!");
+		resp.setContentType("text/html;charset=UTF-8");
 		PrintWriter out = resp.getWriter();
-		out.print("<html><head><title>Test</title></head>");
-		out.print("<body><h1>have a nice day!!</h1></body>");
-		out.print("</html>");
+		out.print("<h1>좋은 하루!</h1>");
 		out.close();
 	}
-
 }
